6f0054c9d348f483d5b6cdad38b902dec38c7034
Paul McClarron <paul.mcclarron@gmail.com>
2022-01-10 00:01:49 +0800

    tooltip styling updates, add herbalist

--- src/components/DieNet/Face.js
+++ src/components/DieNet/Face.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import * as styles from './DieNet.module.scss';
 
-const Face = ({ face, heroType }) => {
+const Face = ({ face, heroType, tooltipDirection }) => {
   const { value, effect } = face || {};
   const { description, jsonId, hasValue, references } = effect || {};
 
   return (
-    <div className={`snd-hero-die-face-${heroType} ${styles.faceShown}`}>
-      {face === null ? 'X' : <div dangerouslySetInnerHTML={{ __html: description }} />}
+    <div className={`snd-hero-die-face-${heroType} ${face ? styles.faceShown : styles.faceBlank}`}>
+      {face === null ? (
+        'X'
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: description }} className={styles.inner} />
+      )}
     </div>
   );
 };
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -4,15 +4,16 @@ export function mapEdgesToNodes(data) {
 }
 
 export function formatFacesEffectsDescription(faces) {
-  faces.forEach((face) => {
+  faces.forEach((face, idx) => {
     if (face) {
       if (face.effect.hasValue) {
         face.effect.description = face.effect.description.replace('{value}', face.value);
       }
       if (face.effect.references) {
+        const tooltipClass = idx === 1 ? 'has-tooltip-right' : 'has-tooltip-top';
         face.effect.references.forEach((ref) => {
           const oldSubstr = `class="keyword-${ref.jsonId}"`;
-          const newSubstr = `class="keyword-${ref.jsonId}" data-tooltip="${ref.jsonId}: ${ref.description}"`;
+          const newSubstr = `class="keyword-${ref.jsonId} ${tooltipClass}" data-tooltip="${ref.jsonId}: ${ref.description}"`;
           face.effect.description = face.effect.description.replace(oldSubstr, newSubstr);
         });
       }
