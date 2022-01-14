0372cf244f03119a9285ee3bb8c9493eae889993
Paul McClarron <paul.mcclarron@gmail.com>
2022-01-14 19:16:29 +0800

    items section layout & initial item for each level

--- src/components/ItemsSection.js
+++ src/components/ItemsSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import { Heading } from 'react-bulma-components';
+import * as styles from './HeroesSection.module.scss';
 
 const ItemsSection = (props) => {
   const { items, headingSize, linkTitle } = props;
@@ -17,13 +18,20 @@ const ItemsSection = (props) => {
   return (
     <>
       {heading}
-      <div className="is-flex">
-        {items.map((item) => (
-          <Link key={item.jsonId} to={`/item/${item.jsonId}/`} className="snd-hero-link-item">
-            {item.name}
-          </Link>
-        ))}
-      </div>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((level) => (
+        <div key={level}>
+          <Heading size={4}>Level {level}</Heading>
+          <div className={`${styles.levelRow} is-flex is-flex-wrap-wrap`}>
+            {items
+              .filter((item) => item.level === level)
+              .map((item) => (
+                <Link key={item.jsonId} to={`/item/${item.jsonId}/`} className="snd-hero-link-item">
+                  {item.name}
+                </Link>
+              ))}
+          </div>
+        </div>
+      ))}
     </>
   );
 };
